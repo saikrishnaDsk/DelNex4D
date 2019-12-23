3bfc1d5a7e38a4fca5eb0ed39b1b26522a3b073a
Saikrishna_Dsk <saikrishnadosapati@gmail.com>
2019-12-23 02:14:14 -0800

    pro1

---
+++ count.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+  int c = 0, vowels = 0,cons=0;
+  char s[1000];
+  gets(s);
+
+  while (s[c] != '\0') {
+    if (s[c] == 'a' || s[c] == 'A' || s[c] == 'e' || s[c] == 'E' || s[c] == 'i' || s[c] == 'I' || s[c] =='o' || s[c]=='O' || s[c] == 'u' || s[c] == 'U')
+      vowels++;
+	else
+	  cons++;
+	c++;
+  }
+  printf("%s",strrev(s));
+  printf("Number of vowels and consonants in the string: %d %d",vowels,cons);
+  
+}
+
