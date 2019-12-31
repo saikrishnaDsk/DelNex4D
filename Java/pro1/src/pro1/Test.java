76df1488c83c529b696f4c46b30f21135343846a
Saikrishna_Dsk <saikrishnadosapati@gmail.com>
2019-12-31 14:05:08 +0530

    Java commit

---
+++ Java/pro1/src/pro1/Test.java
@@ -0,0 +1,25 @@
+package pro1;
+
+public class Test {
+
+	public void hello() {
+		System.out.println("Method Called");
+	}
+	public Test() {
+		System.out.println("Default Const called");
+	}
+	public static void main(String[] args) {
+		Test t = new Test();
+		t.hello();
+		
+		StringBuilder sb = new StringBuilder("Hello");
+		StringBuilder sb1 = new StringBuilder("Hello");
+//		sb.append("World");
+		
+		if(sb.equals(sb1)) {
+			System.out.println("Equal");
+			
+		}
+		
+}
+}
---
+++ Java/pro1/src/pro1/inc.java
@@ -0,0 +1,18 @@
+package pro1;
+
+public class inc {
+
+
+	public static void main(String[] args) {
+		for(char i='a';i<='z';++i) {
+			System.out.print(""+i);
+			
+		}
+		System.out.println("\n");
+		char i ='a';
+		while(i<='z') {
+			System.out.print(""+i);
+			i++;
+		}
+	}
+}
